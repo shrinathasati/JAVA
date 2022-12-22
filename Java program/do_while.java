f135ec12724f8cae6164ddf72aeb1048b5ddc102
Shrinath Asati <120880974+shrinathasati@users.noreply.github.com>
2022-12-22 23:12:50 +0530

    Create do_while.java

---
+++ Java program/do_while.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class do_while {
+    public static void main(String[] args) {
+        int i=1;
+        do{
+            System.out.print(5*i+" ");
+            i++;
+        }while(i<=10);
+    }
+}
